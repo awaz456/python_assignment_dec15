21f29f811a468f4c7e72ae1e884de158c27a725b
awaz456 <awazstha890@gmail.com>
2017-12-18 09:50:48 +0545

    max and min

---
+++ max_min.py
@@ -0,0 +1,8 @@
+my_list = [4,2,4,0,2,4,5,7,8,9,23,8,5,4,2,2,34,4,45]
+my_list.sort()
+
+maximum = my_list[0]
+print("The maximum number is {}".format(maximum))
+
+minimum = my_list[18]
+print("The minimum number is {}".format(minimum))
